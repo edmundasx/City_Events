fdcc19fa23dca95dd660f3eabf3efa574a86e9f0
Vardas <my@email.com>
2025-12-24 23:11:42 +0300

    Initial commit

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+﻿<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+final class HomeController
+{
+  public function index(): void
+  {
+    // Later you will render a view here (from src/Views)
+    echo "CityEvents MVC is running.";
+  }
+}
